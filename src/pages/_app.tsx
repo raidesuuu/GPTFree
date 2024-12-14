3cf361d2fcbe87ad34edc2d77b7ca9eb31a6bf4f
raidesuuu <contact@raic.tech>
2024-12-14 20:39:19 +0900

    chore: Remove unused SVG files and enhance meta tags in _app.tsx
    - Deleted several unused SVG files (file.svg, globe.svg, next.svg, vercel.svg, window.svg) to clean up the project.
    - Updated _app.tsx to include additional meta tags for improved SEO and social sharing, including Open Graph and Twitter card metadata.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,36 @@
 import { AppProps } from "next/app";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <link rel="icon" sizes="32x32" href="favicon.ico" />
-      <title>AI Playground</title>
+    <>
+      <Head>
+        <link rel="icon" sizes="32x32" href="favicon.ico" />
+        <title>AI Playground</title>
+        <meta
+          name="description"
+          content="AI Playgroundは、GPT-4o、o1、Gemini 1.5 Pro、Claude 3.5 SonnetなどのAIをすべて無制限に無料で使用できるサイトです。"
+        />
+        <meta property="og:title" content="AI Playground" />
+        <meta
+          property="og:description"
+          content="AI Playgroundは、GPT-4o、o1、Gemini 1.5 Pro、Claude 3.5 SonnetなどのAIをすべて無制限に無料で使用できるサイトです。"
+        />
+        <meta property="og:image" content="https://ai.raic.dev/card.png" />
+        <meta property="og:url" content="https://ai.raic.dev/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@raic_dev" />
+        <meta name="twitter:title" content="AI Playground" />
+        <meta
+          name="twitter:description"
+          content="AI Playgroundは、GPT-4o、o1、Gemini 1.5 Pro、Claude 3.5 SonnetなどのAIをすべて無制限に無料で使用できるサイトです。"
+        />
+        <meta name="twitter:image" content="https://ai.raic.dev/card.png" />
+      </Head>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
 
